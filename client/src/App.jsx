be3c26336c59549ab8b8c5846ed1ab5f586a8c03
Navin-15 <naveenidrish203@gmail.com>
2025-12-12 20:21:52 +0530

    tenth1.1 commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Topbar from "./components/Topbar";
 import Slider from "./Slider/Slider";
 import FreeResource from './free/FreeResource';
 import Batches from './Batches/Batches'
-import Centers from './center/Centers';
+import Centers from './Center/Centers';
 import Download from './downlaod/Download';
 import './App.css'
 
