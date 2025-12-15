3e7ab703b1c325937710af4f4f8ba561ec64fcd5
Navin-15 <naveenidrish203@gmail.com>
2025-12-15 14:05:17 +0530

    sixteenth.6 commit

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Slider from '../slider/Slider'
 import Batches from '../batches/Batches'
 import Center from '../center/Centers'
 import Free from '../free/FreeResource'
-import Topbar from "../components/Topbar/TopBar";
+import Topbar from '../components/Topbar/Topbar'
 
 const Home = () => {
   return (
